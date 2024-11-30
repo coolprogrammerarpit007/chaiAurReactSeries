682d1b50977670a8bc2a726a3834996fed9d9796
Arpit Mishra <arpit.mishra@microprixs.in>
2024-11-30 16:29:59 +0530

    React Components Building

--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Chai from "./Chai";
+import Pos from "./Pos";
 
 function App() {
 
   return (
     <>
       <h1>React Fundamentals</h1>
+      <Chai/>
+      <p>React Frontend development.</p>
+      <Pos/>
     </>
   );
 }
---
+++ 01-vite-react/src/Pos.jsx
@@ -0,0 +1,8 @@
+function Pos()
+{
+    return (
+        <p>This is a Point on Sell Project Website.</p>
+    );
+}
+
+export default Pos;
